68528b586933e41825b494659fabbe42165ff97c
RaJharit77 <hei.rajo.2@gmail.com>
2025-06-24 10:33:18 +0300

    feat: Add AdditionController and AdditionTest

---
+++ src/test/java/com/arith/demo/unit/AdditionTest.java
@@ -0,0 +1,20 @@
+package com.company.demo.unit;
+
+import static org.junit.Assert.assertEquals;
+
+import com.company.demo.endpoint.rest.controller.AdditionController;
+import org.junit.Test;
+
+public class AdditionTest {
+  AdditionController addition = new AdditionController();
+
+  @Test
+  public void addition_test() {
+    assertEquals(3, addition.addition(1, 2));
+  }
+
+  @Test
+  public void addition_test_no_overflow() {
+    assertEquals(3_000_000_000L, addition.addition(1_000_000_000, 2_000_000_000));
+  }
+}
